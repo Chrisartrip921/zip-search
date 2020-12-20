eb2bf287ab1b7003702548d670a9d09c2f0e70a2
Christopher Artrip <christopherartrip@christophersmbp.home>
2020-12-20 16:09:30 -0500

    First Commit

---
+++ PreWorkTipCalc/Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Christopher Artrip on 12/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PreWorkTipCalc/Shared/PreWorkTipCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreWorkTipCalcApp.swift
+//  Shared
+//
+//  Created by Christopher Artrip on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct PreWorkTipCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
