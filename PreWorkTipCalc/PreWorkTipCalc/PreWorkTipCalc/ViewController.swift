82bbab97db4b87349ec0b1e5172094b5ba8bc06e
Christopher Artrip <christopherartrip@christophersmbp.home>
2020-12-22 00:18:44 -0500

    Added split total feature, added Tip Calculator label

--- PreWorkTipCalc/PreWorkTipCalc/PreWorkTipCalc/ViewController.swift
+++ PreWorkTipCalc/PreWorkTipCalc/PreWorkTipCalc/ViewController.swift
@@ -8,34 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //TExt Field for user to enter bill
     @IBOutlet weak var billAmountTextField: UITextField!
-    
+    //Text field for the amount of people paying
+    @IBOutlet weak var numOfPeople: UITextField!
+    //Presents the price of the tip
     @IBOutlet weak var tipPercentageLabel: UILabel!
-    
+    //Presents the total after adding tip to the bill
     @IBOutlet weak var totalLabel: UILabel!
+    //Segment control for the tip percentage
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var splitTotalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
     }
-
+    
     @IBAction func onTap(_ sender: Any) {
     }
+    
     @IBAction func calculateTip(_ sender: Any) {
         //Get initial bill amount and calculate tips
         let bill = Double(billAmountTextField.text!) ?? 0
+        let people = Double(numOfPeople.text!) ?? 0
         let tipPercentages = [0.15, 0.18, 0.2]
         
         //calculate the total with tip
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
-        //Update the tip and total labels
+        //Calculate the value of the tip after split
+        let splitTotal = ((bill + tip) / people)
+        
+        //Update the tip label
         tipPercentageLabel.text = String(format: "$%.2f", tip)
+        //Update the total label
         totalLabel.text = String(format: "$%.2f", total)
+        //Update the split total label
+        splitTotalLabel.text = String(format: "$%.2f", splitTotal)
+        
     }
     
 }
